282ddb8e5306d6ee78d951eb5942393c9113c00a
William-sqrx <william@sqrx.com>
2025-03-19 13:01:12 +0800

    Initial commit

---
+++ AliceInBoredomLand/AliceInBoredomLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliceInBoredomLandApp.swift
+//  AliceInBoredomLand
+//
+//  Created by Wijaya William on 19/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AliceInBoredomLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
